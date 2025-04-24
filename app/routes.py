91a2479718a58bd78b6f55b8e56a656362ffa7cc
botto8502 <benjamin.j.otto@icloud.com>
2025-04-24 10:50:06 -0500

    add user html and route

--- app/routes.py
+++ app/routes.py
@@ -60,3 +60,13 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title="Register", form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {'author': user, 'body': "First post text"},
+        {'author': user, 'body': "Second post text"}
+    ]
+    return render_template('user.html', user=user, posts=posts)
